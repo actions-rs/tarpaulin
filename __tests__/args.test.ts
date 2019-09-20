2441bf63d06ccb36ca960c4775306298d8e175f9
Gary Tierney <gary.tierney@fastmail.com>
2019-09-20 21:24:51 +0100

    Add simple action inputs test

--- __tests__/args.test.ts
+++ __tests__/args.test.ts
@@ -1,13 +1,25 @@
-import * as args from '../src/args'
+import {describe} from "jest-circus";
+import getActionInputs from "../src/args";
 
 const testEnvVars = {
-    INPUT_VERSION: '0.8.6'
+    INPUT_VERSION: '0.8.6',
+    INPUT_RUN_TYPES: 'Doctests',
+    INPUT_GITHUB_RELEASE_ENDPOINT: 'https://api.github.com/repos/xd009642/tarpaulin/releases'
 };
 
 describe('actions-rs/tarpaulin', () => {
     beforeEach(() => {
+        console.log(testEnvVars);
         for (const key in testEnvVars) {
             process.env[key] = testEnvVars[key as keyof typeof testEnvVars]
         }
+    });
+
+    it('action inputs should be resolved from env vars', async () => {
+       const input = getActionInputs();
+
+       expect(input.runType).toBe('Doctests');
+       expect(input.requestedVersion).toBe('0.8.6');
+       expect(input.releaseEndpoint).toBe('https://api.github.com/repos/xd009642/tarpaulin/releases');
     });
 });
