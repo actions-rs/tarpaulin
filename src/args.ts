08a981c55b374a4ed908a3cde5fc1e5cf7ba8eea
Gary Tierney <gary.tierney@fastmail.com>
2019-09-24 21:01:47 +0100

    Use @actions-rs/core to get input

--- src/args.ts
+++ src/args.ts
@@ -1,20 +1,4 @@
-import * as core from '@actions/core';
-
-// Workaround for a GH bug: https://github.com/actions/toolkit/issues/127
-//
-// For input `all-features: true` it will generate the `INPUT_ALL-FEATURES: true`
-// env variable, which looks too weird.
-// Here we are trying to get proper name `INPUT_NO_DEFAULT_FEATURES` first,
-// and if it does not exist, trying the `INPUT_NO-DEFAULT-FEATURES`
-function getInput(name: string, options?: core.InputOptions): string {
-    const inputFullName = name.replace(/-/g, '_');
-    let value = core.getInput(inputFullName, options);
-    if (value.length > 0) {
-        return value
-    }
-
-    return core.getInput(name)
-}
+import {input} from '@actions-rs/core';
 
 export interface ActionInputs {
     requestedVersion: string,
@@ -24,10 +8,10 @@ export interface ActionInputs {
 }
 
 export default function getActionInputs(): ActionInputs {
-    const requestedVersion = getInput('version');
-    const releaseEndpoint = getInput('github-release-endpoint');
-    const runType = getInput('run-types');
-    const timeout = getInput('timeout');
+    const requestedVersion = input.getInput('version');
+    const releaseEndpoint = input.getInput('github-release-endpoint');
+    const runType = input.getInput('run-types');
+    const timeout = input.getInput('timeout');
 
     return {
         requestedVersion,
