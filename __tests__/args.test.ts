89e6e34e77412b80e9543e3937ce98ad25bd6e28
Wesley Klop <wesley19097@gmail.com>
2020-04-21 20:49:57 +0200

    Add new option to tests

--- __tests__/args.test.ts
+++ __tests__/args.test.ts
@@ -1,23 +1,25 @@
-import {describe} from "jest-circus";
+import { describe } from "jest-circus";
 import getActionInputs from "../src/args";
 
 const testEnvVars = {
     INPUT_VERSION: '0.8.6',
     INPUT_RUN_TYPES: 'Doctests',
+    INPUT_OUT_TYPE: 'Lcov',
 };
 
 describe('actions-rs/tarpaulin', () => {
     beforeEach(() => {
         console.log(testEnvVars);
-        for (const key in testEnvVars) {
-            process.env[key] = testEnvVars[key as keyof typeof testEnvVars]
-        }
+        Object.entries(testEnvVars).forEach(([key, value]) => {
+            process.env[key] = value
+        })
     });
 
     it('action inputs should be resolved from env vars', async () => {
-       const input = getActionInputs();
+        const input = getActionInputs();
 
-       expect(input.runType).toBe('Doctests');
-       expect(input.requestedVersion).toBe('0.8.6');
+        expect(input.runType).toBe('Doctests');
+        expect(input.requestedVersion).toBe('0.8.6');
+        expect(input.outType).toBe('Lcov')
     });
 });
