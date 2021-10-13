45409225be926f8ad4108170ef927cce9a1bbf67
eleanor <git@eleanor.helly.email>
2021-10-13 17:16:57 -0600

    Fix error is unknown error in main()

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function main() {
     try {
         await run();
     } catch (error) {
-        core.setFailed(error.message);
+        core.setFailed((error as Error).message);
     }
 }
 
